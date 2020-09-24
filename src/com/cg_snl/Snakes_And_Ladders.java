bf55adcaf57ce3356cb0a3407de1fa56bba04f47
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-24 16:29:56 +0530

    refactored UC1

--- src/com/cg_snl/Snakes_And_Ladders.java
+++ src/com/cg_snl/Snakes_And_Ladders.java
@@ -1,9 +1,17 @@
 package com.cg_snl;
 
+import java.util.Random;
+
 public class Snakes_And_Ladders {
 	
 	public static void main(String[] args) {
 		int pos=0;
+		
+		Random rand =new Random();
+		int dice=rand.nextInt(6)+1;
+		
+		System.out.println("No. on Dice: "+dice);
+		
 	}
 
 }
