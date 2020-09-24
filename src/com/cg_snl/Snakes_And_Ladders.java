804c9c1e32323f1f990add4493664a76fdb8b213
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-24 16:40:06 +0530

    refactored UC3

--- src/com/cg_snl/Snakes_And_Ladders.java
+++ src/com/cg_snl/Snakes_And_Ladders.java
@@ -10,7 +10,23 @@ public class Snakes_And_Ladders {
 		Random rand =new Random();
 		int dice=rand.nextInt(6)+1;
 		
-		System.out.println("No. on Dice: "+dice);
+		int option=rand.nextInt(3);
+		switch (option) {
+		case 0:
+			System.out.println("No Play.");
+			break;
+		case 1:
+			System.out.println("Ladder.");
+			pos=pos+dice;
+			break;
+		case 2:
+			System.out.println("Snake.");
+			pos=pos-dice;
+			break;
+			
+		}
+		System.out.println("New Position: "+ pos);
+		
 		
 	}
 
