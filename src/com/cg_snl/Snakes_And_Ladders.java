a1c65b50eec761465a82af9e13341b16a084bb53
siddharthagupta20 <siddharthagupta20@gmail.com>
2020-09-24 18:05:20 +0530

    refactored UC6

--- src/com/cg_snl/Snakes_And_Ladders.java
+++ src/com/cg_snl/Snakes_And_Ladders.java
@@ -5,44 +5,90 @@ import java.util.Random;
 public class Snakes_And_Ladders {
 	
 	public static void main(String[] args) {
-		int pos=0;
 		
+		int pos1=0, pos2=0;
 		Random rand =new Random();
-		int turn=0;
-		while(pos!=100) {
-		int dice=rand.nextInt(6)+1;
+		boolean player1Playing=true;
 		
-		int option=rand.nextInt(3);
+		while(pos1!=100&& pos2!=100) {	
+		
+		boolean onLadder=true;
+		
+		if(player1Playing) {
+			
+		int lastRoll=0;
+		
+		while(onLadder) {
+			int dice=rand.nextInt(6)+1;
+			int option=rand.nextInt(3);
+			
 		switch (option) {
+		
 		case 0:
 			
+			onLadder=false;
 			break;
+			
 		case 1:
 			
-			pos=pos+dice;
+			lastRoll=dice;
+			pos1+=lastRoll;
 			break;
+			
 		case 2:
 			
-			pos=pos-dice;
-			break;
-			
+			pos1=pos1-dice;
+			onLadder=false;
+			break;	
 		}
-		if (pos>100) {
-			pos-=dice;
-			
 		}
-		
-		if (pos<0) 
-			pos=0;
-		
-		System.out.println("New Position: "+ pos);
-		if (pos==100) {
+		if (pos1>100) 
+			pos1-=lastRoll;
+		if (pos1<0) 
+			pos1=0;
+		if (pos1==100) {
+			System.out.println("Player 1 Won.");
 			break;
 		}
-	
-		turn++;
+		player1Playing=false;
+		
+		
 		}
-		System.out.println("Turns: "+turn);
+		else {
+		
+			int lastRoll=0;
+			while(onLadder) {
+				int dice=rand.nextInt(6)+1;
+				int option=rand.nextInt(3);
+			switch (option) {
+			case 0:
+				onLadder=false;
+				break;
+			case 1:
+				lastRoll=dice;
+				pos2=pos2+lastRoll;
+				break;
+			case 2:
+				onLadder=false;
+				pos2=pos2-dice;
+				break;
+			}
+			}
+			if (pos2>100) 
+				pos2-=lastRoll;
+			if (pos2<0) 
+				pos2=0;
+			if(pos2==100){
+				System.out.println("Player 2 Won.");
+				break;
+			}
+			player1Playing=true;
+		
+			
+		}
+		}
+		
 	}
 
 }
+
